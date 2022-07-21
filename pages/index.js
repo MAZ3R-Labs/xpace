61c6a9081c4d334e997d50a6c884ec4a64220499
MAZ3R-Labs <50402066+MAZ3R-Labs@users.noreply.github.com>
2022-07-22 01:21:45 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -99,9 +99,7 @@ export default function Home() {
             </div>
             <div className={styles.link_bar} />
           </div>
-        <div>
-          <p className="text-white">style</p>
-        </div>
+        
         </div>
       </section>
     </div>
