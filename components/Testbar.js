5200e9a377c16e177d52559d0511f0427317ccfd
MAZ3R-Labs <50402066+MAZ3R-Labs@users.noreply.github.com>
2022-07-21 18:26:05 +0800

    navbar rwd

--- components/Testbar.js
+++ components/Testbar.js
@@ -1,58 +1,158 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
 
-function NavLink({to, children}) {
-    return <a href={to} className={`mx-4`}>
-        {children}
+function NavLink({ to, children }) {
+  return (
+    <a href={to} className={`mx-4 text-white`}>
+      {children}
     </a>
+  );
 }
 
-function MobileNav({open, setOpen}) {
-    return (
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-lg `}>
-            <div className="flex items-center justify-center filter drop-shadow-lg bg-white h-20"> {/*logo container*/}
-                <a className="text-xl font-semibold" href="/">LOGO</a>
-            </div>
-            <div className="flex flex-col ml-4 text-black">
-                <a className="text-xl font-medium my-4" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    About
-                </a>
-                <a className="text-xl font-normal my-4" href="/contact" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Contact
-                </a>
-            </div>  
+function MobileNav({ open, setOpen }) {
+  return (
+    <div
+      className={`absolute top-0 left-0 h-screen w-screen bg-black transform ${
+        open ? "-translate-x-0" : "-translate-x-full"
+      } transition-transform duration-300 ease-in-out filter drop-shadow-lg `}
+    >
+      <div className="flex items-center justify-center filter drop-shadow-lg text-white bg-black h-20">
+        {/*logo container*/}
+      </div>
+      <div className="flex justify-end mr-6">
+        <div className="flex flex-col">
+          <Link href="/chat">
+            <a
+              className="text-xl my-4 text-white font-semibold"
+              onClick={() =>
+                setTimeout(() => {
+                  setOpen(!open);
+                }, 100)
+              }
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="/explore">
+            <a
+              className="text-xl my-4 text-white font-semibold"
+              onClick={() =>
+                setTimeout(() => {
+                  setOpen(!open);
+                }, 100)
+              }
+            >
+              BBS X-city
+            </a>
+          </Link>
+          <Link href="/premium">
+            <a
+              className="text-xl my-4 text-white font-semibold"
+              onClick={() =>
+                setTimeout(() => {
+                  setOpen(!open);
+                }, 100)
+              }
+            >
+              X-pet
+            </a>
+          </Link>
+          <Link href="/about">
+            <a
+              className="text-xl my-4 text-white font-semibold"
+              onClick={() =>
+                setTimeout(() => {
+                  setOpen(!open);
+                }, 100)
+              }
+            >
+              XPA DAO
+            </a>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default function Navbar() {
-    
-    const [open, setOpen] = useState(false)
-    return (
-        <nav className="flex filter drop-shadow-lg bg-white px-4 py-4 h-20 items-center">
-            <MobileNav open={open} setOpen={setOpen}/>
-            <div className="w-3/12 flex items-center">
-                <a className="text-2xl font-semibold" href="/">LOGO</a>
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="flex justify-between filter drop-shadow-lg bg-black px-4 py-4 items-center">
+      <MobileNav open={open} setOpen={setOpen} />
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="mr-24 ml-20 self-center">
+            <a>
+              <Image src="/Logo.svg" width={87} height={17} />
+            </a>
+          </div>
+        </Link>
+        {/* desktop Navbar */}
+        <div className="hidden lg:flex">
+          <Link href="/chat">
+            <div className="mr-10 self-center">
+              <a className="text-white hover:text-cyan-300 font-semibold">
+                Home
+              </a>
             </div>
-            <div className="w-9/12 flex justify-end items-center">
-
-                <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center lg:hidden" onClick={() => {
-                    setOpen(!open)
-                }}>
-                    {/* hamburger button */}
-                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-                    <span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-                    <span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
-                </div>
-
-                <div className="hidden lg:flex">
-                    <NavLink to="/contact">
-                        CONTACT
-                    </NavLink>
-                    <NavLink to="/about">
-                        ABOUT
-                    </NavLink>
-                </div>
+          </Link>
+          <Link href="/explore">
+            <div className="mr-10 self-center">
+              <a className="text-white hover:text-cyan-300 font-semibold">
+                BBS X-city
+              </a>
             </div>
-        </nav>
-    )
+          </Link>
+          <Link href="/premium">
+            <div className="mr-10 self-center">
+              <a className="text-white hover:text-cyan-300 font-semibold">
+                X-pet
+              </a>
+            </div>
+          </Link>
+          <Link href="/about">
+            <div className="mr-10 self-center">
+              <a className="text-white hover:text-cyan-300 font-semibold">
+                XPA DAO
+              </a>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      <div className=" flex justify-end items-center">
+      <Link href="/login">
+          <div className=" mr-10 lg:mr-20 self-center font-semibold">
+            <a className={styles.login}>Follow Us</a>
+          </div>
+        </Link>
+        <div
+          className="z-50 flex relative w-8 h-8 flex-col justify-between items-center lg:hidden"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {/* hamburger button */}
+          <span
+            className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "rotate-45 translate-y-3.5" : ""
+            }`}
+          />
+          <span
+            className={`h-1 w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${
+              open ? "w-0" : "w-full"
+            }`}
+          />
+          <span
+            className={`h-1 w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "-rotate-45 -translate-y-3.5" : ""
+            }`}
+          />
+        </div>
+      </div>
+    </nav>
+  );
 }
