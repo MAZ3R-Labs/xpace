5dd9451719617bf1716109df96076eef85772a22
MAZ3R-Labs <50402066+MAZ3R-Labs@users.noreply.github.com>
2022-07-27 20:33:56 +0800

    item padding

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,41 +14,40 @@ const Navbar = () => {
             <Link href="/">
               <div className="absolute w-24 ml-6 md:mr-24 md:ml-20 self-center object-none">
                 <a>
-                 <img src="/logo/LOGO.svg" className=" object-none " />
+                  <img src="/logo/LOGO.svg" className=" object-none " />
                 </a>
               </div>
             </Link>
-          <div className="ml-48 hidden md:flex">
-            <Link href="/chat">
-              <div className="mr-10 self-center">
-                <a className="text-white hover:text-cyan-300 font-semibold">
-                  Home
-                </a>
-              </div>
-            </Link>
-            <Link href="/explore">
-              <div className="mr-10 self-center">
-                <a className="text-white hover:text-cyan-300 font-semibold">
-                  BBS X-city
-                </a>
-              </div>
-            </Link>
-            <Link href="/premium">
-              <div className="mr-10 self-center">
-                <a className="text-white hover:text-cyan-300 font-semibold">
-                  X-pet
-                </a>
-              </div>
-            </Link>
-            <Link href="/about">
-              <div className="mr-10 self-center">
-                <a className="text-white hover:text-cyan-300 font-semibold">
-                  XPA-DAO
-                </a>
-              </div>
-            </Link>
+            <div className="ml-48 hidden md:flex">
+              <Link href="/chat">
+                <div className="mr-10 self-center">
+                  <a className="text-white hover:text-cyan-300 font-semibold">
+                    Home
+                  </a>
+                </div>
+              </Link>
+              <Link href="/explore">
+                <div className="mr-10 self-center">
+                  <a className="text-white hover:text-cyan-300 font-semibold">
+                    BBS X-city
+                  </a>
+                </div>
+              </Link>
+              <Link href="/premium">
+                <div className="mr-10 self-center">
+                  <a className="text-white hover:text-cyan-300 font-semibold">
+                    X-pet
+                  </a>
+                </div>
+              </Link>
+              <Link href="/about">
+                <div className="mr-10 self-center">
+                  <a className="text-white hover:text-cyan-300 font-semibold">
+                    XPA-DAO
+                  </a>
+                </div>
+              </Link>
             </div>
-            
           </div>
           <div className="navbar-end md:hidden">
             <Link href="/login">
@@ -100,25 +99,50 @@ const Navbar = () => {
       </nav>
       {toggleMenu && (
         <div className=" fixed top-[60px] left-1/2 z-30 w-1/2 h-screen py-[34px] px-[16px] flex flex-col justify-start items-end md:hidden text-white bg-black">
-          <label
-            htmlFor="my-modal-1"
-            className=" modal-button hover:underline font-semibold text-base mb-9"
-          >
-            Storyline
-          </label>
-          <a
-            href={`#faq`}
-            className="font-semibold text-base mb-9"
-            onClick={() => {
-              setToggleMenu(false);
-              enableScroll();
-            }}
-          >
-            FAQ & Tutorials
-          </a>
-
-         
-          <h1 className="font-semibold text-base mb-8">Dashbosrd</h1>
+          <Link href="/chat">
+            <a
+              className="font-semibold text-base mb-9"
+              onClick={() => {
+                setToggleMenu(false);
+                enableScroll();
+              }}
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="/explore">
+            <a
+              className="font-semibold text-base mb-9"
+              onClick={() => {
+                setToggleMenu(false);
+                enableScroll();
+              }}
+            >
+              BBS X-city
+            </a>
+          </Link>
+          <Link href="/premium">
+            <a
+              className="font-semibold text-base mb-9"
+              onClick={() => {
+                setToggleMenu(false);
+                enableScroll();
+              }}
+            >
+              X-pet
+            </a>
+          </Link>
+          <Link href="/about">
+            <a
+              className="font-semibold text-base mb-9"
+              onClick={() => {
+                setToggleMenu(false);
+                enableScroll();
+              }}
+            >
+              XPA-DAO
+            </a>
+          </Link>
         </div>
       )}
     </>
