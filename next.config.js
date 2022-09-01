69a58821d7d941fd630f5e863bb8d0708be4e748
cshung1994 <cshung1994@gmail.com>
2022-09-01 17:39:44 +0800

    update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+        // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
